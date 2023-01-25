27acb61a1f4865b0060eae2ef14613538bbfff81
vhraposo <thevhup@gmail.com>
2023-01-25 11:55:34 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+import Controls  from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import {
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonSet,
+    minutesDisplay,
+    secondsDisplay
+} from "./elements.js"
+
+
+
+
+const controls = Controls({
+    buttonPlay,
+    buttonPause,
+    buttonSet,
+    buttonStop})
+
+//Injeção de dependencias
+const timer = Timer({
+    minutesDisplay, secondsDisplay, resetControls : controls.reset
+})
+
+
+const sound = Sound()
+
+Events({controls, timer, sound})
+
+
+
+
+
+
+
